d05e014965fd9e081a91c958d6a9f4edcc95d34e
Daniel Xavier <dxavier194@gmail.com>
2023-06-29 20:58:00 -0300

    Bootstrap 5

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,8 +6,8 @@
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>{{ $title }} - Controle de Clientes</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <style>
     body {
         background-color: #e8eff1;
